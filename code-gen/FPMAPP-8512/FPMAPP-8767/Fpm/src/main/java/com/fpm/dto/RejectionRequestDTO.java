2e86fdb1d11511a23613fced099467783b01e296
Anubroto <anubroto.g@prodapt.com>
2026-03-27 18:29:50 +0530

    Code gen: FPMAPP-8767 — Fpm/src/main/java/com/fpm/dto/RejectionRequestDTO.java

---
+++ code-gen/FPMAPP-8512/FPMAPP-8767/Fpm/src/main/java/com/fpm/dto/RejectionRequestDTO.java
@@ -0,0 +1,23 @@
+package com.fpm.dto;
+
+public class RejectionRequestDTO {
+
+    private String userId;
+    private String comments;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
